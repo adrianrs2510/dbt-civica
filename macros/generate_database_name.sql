025740f1db9b4e43bff2aaa71bf4e681faa06a76
Adrián Ruiz Sánchez <adrianrs2510@gmail.com>
2026-05-04 17:53:41 +0000

    marts --> Capa Gold :)

--- macros/generate_database_name.sql
+++ macros/generate_database_name.sql
@@ -1,9 +1,9 @@
---macros/generate_database_name.sql--nombre EXACTO obligatorio
+--macros/generate_database_name.sql
 {% macro generate_database_name(
     custom_database_name, node
 ) -%}
     {%-if custom_database_name
-        is not none -%}
+    is not none -%}
         {{ custom_database_name | trim }}
     {%-else -%}
         {{ target.database | trim }}
--- models/staging/postgres/stg_postgres__products_inc_merge.sql
+++ models/staging/postgres/stg_postgres__products_inc_merge.sql
@@ -1,8 +1,7 @@
 {{
     config(
         materialized='incremental',
-        unique_key = 'product_id',
-        incremental_stratregy = 'merge'
+        unique_key = 'product_id'
     )
 }}
 with 
